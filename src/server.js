2daa315e0a47aafff1013fa8b3d8aee9a42f19ff
sdcomms4227 <sdcomms4227@gmail.com>
2025-04-18 13:18:24 +0900

    complete 1st

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+app.use((req, res, next) => res.redirect("/"));
+
+const handleListen = () => console.log("Server is running on http://localhost:3000");
+
+app.listen(3000, handleListen);
